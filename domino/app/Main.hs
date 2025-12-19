969a6fb01f038fe356c8d456c600856ef7dac972
Abel de la Noval <ryuto1234yolo@gmail.com>
2025-12-18 21:40:05 -0500

    Add: Gloss

--- domino/app/Main.hs
+++ domino/app/Main.hs
@@ -1,19 +1,97 @@
 module Main (main) where
 
 import Lib
-import System.IO
+import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+import System.IO (hSetEncoding, stdout, utf8)
+
+-- =========================
+-- ESTADO DEL MUNDO (GLOSS)
+-- =========================
+
+data World = World
+  { worldJugadores :: [Jugador]
+  , worldPozo      :: [Ficha]
+  }
+
+-- =========================
+-- CREACIÓN DEL ESTADO INICIAL
+-- =========================
+
+estadoInicial :: IO World
+estadoInicial = do
+    hSetEncoding stdout utf8  -- opcional ahora, ya no imprimimos
+    let fichas = generarFichas
+    fichasBarajadas <- barajarFichas fichas
+
+    let nJugadores = 4
+    let (jugadores, pozo) = repartirManos fichasBarajadas nJugadores
+
+    pure $ World
+      { worldJugadores = jugadores
+      , worldPozo = pozo
+      }
+
+-- =========================
+-- MAIN
+-- =========================
 
 main :: IO ()
 main = do
-    hSetEncoding stdout utf8
-    let fichas = generarFichas
-    fichasBarajadas <- barajarFichas fichas
-    
-    let nJugadores = 4 -- Por ahora, jugamos con 4 jugadores
-    let (jugadores, pozo) = repartirManos fichasBarajadas nJugadores
+    world <- estadoInicial
+    play
+      (InWindow "Domino" (1100, 700) (100, 100))
+      white
+      60
+      world
+      drawWorld
+      handleEvent
+      updateWorld
 
-    putStrLn "--- Manos de los jugadores ---"
-    mapM_ print jugadores
-    
-    putStrLn "\n--- Fichas restantes en el pozo ---"
-    print pozo
+-- =========================
+-- DIBUJO
+-- =========================
+
+drawWorld :: World -> Picture
+drawWorld world =
+    Pictures $
+        drawJugadores (worldJugadores world)
+        ++ [drawPozo (worldPozo world)]
+
+-- Texto helper
+drawTexto :: Float -> Float -> String -> Picture
+drawTexto x y str =
+    Translate x y $
+    Scale 0.15 0.15 $
+    Text str
+
+-- Dibujar jugadores
+drawJugadores :: [Jugador] -> [Picture]
+drawJugadores jugadores =
+    zipWith drawJugador jugadores [0..]
+
+drawJugador :: Jugador -> Int -> Picture
+drawJugador jugador i =
+    drawTexto (-500) (300 - fromIntegral i * 80) $
+        "Jugador " ++ show (jugadorId jugador)
+        ++ ": " ++ show (mano jugador)
+
+-- Dibujar pozo
+drawPozo :: [Ficha] -> Picture
+drawPozo pozo =
+    drawTexto (-500) (-300) $
+        "Pozo: " ++ show pozo
+
+-- =========================
+-- EVENTOS (por ahora vacíos)
+-- =========================
+
+handleEvent :: Event -> World -> World
+handleEvent _ world = world
+
+-- =========================
+-- ACTUALIZACIÓN (por ahora vacía)
+-- =========================
+
+updateWorld :: Float -> World -> World
+updateWorld _ world = world
--- domino/src/Lib.hs
+++ domino/src/Lib.hs
@@ -1,39 +1,51 @@
 module Lib
-    ( Ficha,
-      generarFichas,
-      barajarFichas,
-      repartirManos,
-      Mano,
-      Jugador
+    ( Ficha(..)
+    , generarFichas
+    , barajarFichas
+    , repartirManos
+    , Mano
+    , Jugador(..)
     ) where
 
 import System.Random (newStdGen)
 import System.Random.Shuffle (shuffle')
 
 -- | Representa una ficha de dominó con dos valores.
-data Ficha = Ficha Int Int deriving (Show, Eq)
+data Ficha = Ficha
+  { ladoA :: Int
+  , ladoB :: Int
+  } deriving (Show, Eq)
+
+-- | Mano de fichas
+type Mano = [Ficha]
 
 -- | Define un Jugador con un ID y su mano de fichas.
-type Mano = [Ficha]
-data Jugador = Jugador { jugadorId :: Int, mano :: Mano } deriving (Show)
+data Jugador = Jugador
+  { jugadorId :: Int
+  , mano      :: Mano
+  } deriving (Show)
 
 -- | Genera el juego completo de fichas de dominó doble-nueve.
 generarFichas :: [Ficha]
-generarFichas = [Ficha i j | i <- [0..9], j <- [i..9]]
+generarFichas =
+  [Ficha i j | i <- [0..9], j <- [i..9]]
 
 -- | Baraja una lista de fichas de forma aleatoria.
--- Devuelve una nueva lista de fichas barajadas y el nuevo generador de números aleatorios.
 barajarFichas :: [Ficha] -> IO [Ficha]
 barajarFichas fichas = do
     gen <- newStdGen
-    return $ shuffle' fichas (length fichas) gen
+    pure $ shuffle' fichas (length fichas) gen
 
 -- | Reparte las fichas barajadas entre un número dado de jugadores.
 -- Por ahora, asume un reparto fijo de 10 fichas por jugador.
 repartirManos :: [Ficha] -> Int -> ([Jugador], [Ficha])
 repartirManos fichas nJugadores =
     let (manos, resto) = splitAt (nJugadores * 10) fichas
-        jugadores = zipWith (\id mano -> Jugador id mano) [1..nJugadores] (chunksOf 10 manos)
+        jugadores =
+          zipWith
+            (\jid m -> Jugador jid m)
+            [1..nJugadores]
+            (chunksOf 10 manos)
     in (jugadores, resto)
 
 -- | Función auxiliar para dividir una lista en trozos de un tamaño dado.
